58cefbc0f6af9f3c050befcd22351106b43fd568
Ville-Pekka Lahti <ville-pekka.lahti@kotivo.fi>
2019-07-16 00:47:09 +0300

    porta

--- src/avr-sound.c
+++ src/avr-sound.c
@@ -18,6 +18,7 @@
 volatile uint16_t avrsound_buffercursor[AVRSOUND_MAX_CHANNELS];
 volatile float avrsound_buffer_jump = 1 >> 8;
 volatile uint16_t avrsound_buffer_speed[AVRSOUND_MAX_CHANNELS];
+volatile uint16_t avrsound_buffer_speed_target[AVRSOUND_MAX_CHANNELS];
 volatile float avrsound_buffer_hz = 440.0;
 volatile uint16_t avrsound_buffer_len = 256;
 volatile int16_t avrsound_buffer[AVRSOUND_MAXIMUM_SAMPLE_LENGTH];
@@ -89,10 +90,10 @@ uint8_t avrsound_get_volume(uint8_t channel) {
 void avrsound_set_hz(uint8_t channel, float hz)
 {
 	if (hz < 8.5) {
-		avrsound_buffer_speed[channel] = 0;
+		avrsound_buffer_speed_target[channel] = 0;
 
 	} else {
-		avrsound_buffer_speed[channel] = finetune * 256.0 * 256.0 * avrsound_buffer_len * hz / (_avrsound_samplerate * avrsound_buffer_hz);
+		avrsound_buffer_speed_target[channel] = finetune * 256.0 * 256.0 * avrsound_buffer_len * hz / (_avrsound_samplerate * avrsound_buffer_hz);
 	}
 
 }
@@ -123,15 +124,33 @@ uint8_t avrsound_compress(uint16_t sample, uint8_t channels) {
 
 
 
-uint16_t abcx16(uint16_t a, uint16_t b, uint16_t c, uint16_t x) {
-	if (a == 65535) 
+uint16_t abx16(uint16_t a, uint16_t b, uint16_t x) {
+	if (b == 65535)
 		return 65535;
-	if (x < a) 
-		return c+(uint32_t)b*(uint32_t)x/a;
-	return (((x-a-c)*(65536-b+c)) / (65536-a)) + b;
+	if (x < b)
+		return (uint32_t)a * (uint32_t)x / b;
+	return (((x - b) * (65536 - a)) / (65536 - b)) + a;
 }
 
+uint8_t bitcrush(uint8_t sample, uint8_t bits) {
+	if (bits >= 8)
+		return sample;
+	return sample & (0xff << (8 - bits));
+}
 
+volatile uint16_t porta_to = 0;
+volatile float porta_step = 0;
+volatile uint16_t portamod = 1;
+volatile uint16_t portacounter = 0;
+
+uint16_t porta(uint16_t from, uint16_t to) {
+	portacounter++;
+	if (portacounter > portamod) {
+		portacounter -= portamod;
+		return (from+from+from+to) >> 2;
+	}
+	return from;
+}
 
 
 
@@ -141,7 +160,7 @@ uint16_t fuzzyY = 32767;
 uint16_t fuzzyZ = 0;
 uint16_t fuzzyAngle = 0;
 
-
+uint8_t bitcrush_amount = 8;
 
 
 
@@ -150,14 +169,19 @@ ISR (TIMER1_COMPA_vect)
 	int16_t bufsum = 0;
 	uint8_t active_channels = 0;
 	for (uint8_t i = 0; i < AVRSOUND_MAX_CHANNELS; i++) {
+		avrsound_buffer_speed[i] = porta(avrsound_buffer_speed[i], avrsound_buffer_speed_target[i]);
 		if (avrsound_buffer_speed[i] > 0) {
 			if (avrsound_using_volume == 1) {
-				bufsum += (avrsound_buffer[abcx16(fuzzyX, fuzzyY, fuzzyZ, avrsound_buffercursor[i]) >> 8] * avrsound_buffer_volume[i] >> 8) + avrsound_buffer_volume_additive[i];
+				bufsum += (avrsound_buffer[abx16(fuzzyX, fuzzyY, avrsound_buffercursor[i]) >> 8] * avrsound_buffer_volume[i] >> 8) + avrsound_buffer_volume_additive[i];
 			} else {
-				bufsum += avrsound_buffer[abcx16(fuzzyX, fuzzyY, fuzzyZ, avrsound_buffercursor[i]) >> 8];
+				bufsum += avrsound_buffer[abx16(fuzzyX, fuzzyY, avrsound_buffercursor[i]) >> 8];
 			}
 
 			avrsound_buffercursor[i] = (avrsound_buffercursor[i] + avrsound_buffer_speed[i]);
+
+
+
+
 			active_channels++;
 		}
 	}
@@ -165,7 +189,7 @@ ISR (TIMER1_COMPA_vect)
 	if (bufsum != 0) {
 		AVRSOUND_PORT_LED |= (1 << AVRSOUND_PIN_LED);
 
-		bufsum = avrsound_compress(bufsum, active_channels);
+		bufsum = bitcrush(avrsound_compress(bufsum, active_channels), bitcrush_amount);
 
 		AVRSOUND_PORT = bufsum;
 
@@ -184,12 +208,13 @@ int16_t fuzzydeltaY = 0, fuzzydeltaX = 0;
 
 ISR (TIMER2_COMPA_vect) {
 	if (adcprescale % 64 == 0) {
-		fuzzydeltaY = (adc_read(1)/4-64) << 1;
-		fuzzydeltaX = (adc_read(2)/4-64) << 1;
-		fuzzyZ = adc_read(3) << 6;
+		bitcrush_amount = 8 - ((adc_read(0) >> 7));
+		fuzzyX = (adc_read(1)) << 6;
+		fuzzyY = (adc_read(2)) << 6;
+		portamod = (adc_read(3) >> 2) + 1;
 	}
-	fuzzyY += fuzzydeltaY;
-	fuzzyX += fuzzydeltaX;
+	//fuzzyY += fuzzydeltaY;
+	//fuzzyX += fuzzydeltaX;
 	adcprescale++;
 }
 
